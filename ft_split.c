15c9f82fe3c87e4f3782d54ff18fe1ddd70c8c90
Mingu Kong <mkong@c3r1s2.42seoul.kr>
2023-10-13 20:45:59 +0900

    split

--- ft_split.c
+++ ft_split.c
@@ -6,41 +6,26 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 16:24:29 by mkong             #+#    #+#             */
-/*   Updated: 2023/10/13 18:21:23 by mkong            ###   ########.fr       */
+/*   Updated: 2023/10/13 20:40:37 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	count_words(char *str, char c)
+#include <stdio.h>
+int	count_words(const char *s, char c)
 {
 	int	cnt;
 
 	cnt = 0;
-	while (str)
+	while (s && *s)
 	{
-		if (str != ft_strchr(str, c))
-			str++;
+		if (s == ft_strchr(s, c))
+			s++;
 		else
 		{
 			cnt++;
-			str = ft_strchr(str, c);
+			s = ft_strchr(s, c);
 		}
-	}
-	return (cnt);
-}
-
-int	count_one_word(char *str, char c)
-{
-	int	cnt;
-
-	cnt = 0;
-	while (str)
-	{
-		while (str == ft_strchr(str, c))
-			str++;
-		cnt++;
-		str = ft_strchr(str, c);
 	}
 	return (cnt);
 }
@@ -48,27 +33,30 @@ int	count_one_word(char *str, char c)
 char	**ft_split(char const *s, char c)
 {
 	char	**words;
-	size_t	s_len;
 	size_t	one_word_len;
+	size_t	index;
 
-	words = (char **)malloc(sizeof(char *) * (count_words(s,c)));
-	s_len = ft_strlen(s);
+	words = (char **)malloc(sizeof(char *) * ((count_words(s, c) + 1)));
+	index = 0;
 	if (words == 0)
 		return (0);
-	while (str)
+	while (s && *s)
 	{
 		if (s == ft_strchr(s, c))
-			str++;
+			s++;
 		else
 		{
 			if (ft_strchr(s, c) == 0)
-				*word = ft_strdup(s);
+				words[index] = ft_strdup(s);
 			else
 			{
-				one_word_len = ft_strchr(s, c) - str;
-				*word = ft_strdup(ft_substr(s, 0, one_word_len));
+				one_word_len = ft_strchr(s, c) - s;
+				words[index] = ft_strdup(ft_substr(s, 0, one_word_len + 1));
 			}
-			word++;
+			s = ft_strchr(s, c);
+			index++;
 		}
 	}
+	words[index] = 0;
+	return (words);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:40:34 by mkong             #+#    #+#             */
-/*   Updated: 2023/10/13 16:23:50 by mkong            ###   ########.fr       */
+/*   Updated: 2023/10/13 19:33:10 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
