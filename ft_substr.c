76c880bc05d4220f395e160e10a171adba86d7bc
gongmg <rhdalsrn2@naver.com>
2023-10-15 16:35:07 +0900

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 15:48:19 by mkong             #+#    #+#             */
-/*   Updated: 2023/10/15 16:05:50 by mkong            ###   ########.fr       */
+/*   Updated: 2023/10/15 16:34:56 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	s_len = ft_strlen(s);
 	result = (char *)malloc(sizeof(char) * len + 1);
-	if (result == 0 || start > len)
+	if (result == 0 || start + len - 1 > s_len)
 		return (0);
 	ft_strlcpy(result, s + start, len);
 	return (result);
