ff744a13ded867a2cf0d3d63eb36e7cb5026e36c
gongmg <rhdalsrn2@naver.com>
2023-11-30 20:09:10 +0900

    bonus_fail

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,22 +6,26 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:41:47 by mkong             #+#    #+#             */
-/*   Updated: 2023/11/29 22:04:57 by mkong            ###   ########.fr       */
+/*   Updated: 2023/11/30 19:59:19 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static char	*free_lst(t_list **lst, char *r_str, int fail)
+static char	*free_lst(t_list **lst, t_list *head, char *r_str, int fail)
 {
 	if (*lst != 0)
 	{
+
 		if ((*lst)->str != 0)
 			free((*lst)->str);
 		(*lst)->str = 0;
 		(*lst)->idx = 0;
 		if (fail)
 		{
+			while (head->next_node && *lst != head->next_node)
+				head = head->next_node;
+			head->next_node = (*lst)->next_node;
 			free(*lst);
 			*lst = 0;
 		}
@@ -31,7 +35,7 @@ static char	*free_lst(t_list **lst, char *r_str, int fail)
 	return (0);
 }
 
-static char	*set_lst(t_list **lst, int fd)
+static char	*set_lst(t_list **lst, t_list *h, int fd)
 {
 	t_list	*new_node;
 
@@ -50,7 +54,9 @@ static char	*set_lst(t_list **lst, int fd)
 		while (new_node->next_node && new_node->fd != fd)
 			new_node = new_node->next_node;
 		if (!new_node->next_node && new_node->fd != fd)
-			set_lst(&new_node->next_node, fd);
+		{
+			set_lst(&new_node->next_node, h, fd);
+		}
 		if (new_node->fd == fd)
 			*lst = new_node;
 		else
@@ -60,41 +66,41 @@ static char	*set_lst(t_list **lst, int fd)
 	{
 		(*lst)->str = (char *)malloc(BUFFER_SIZE + 1);
 		if ((*lst)->str == 0)
-			return (free_lst(lst, 0, 1));
+			return (free_lst(lst, h, 0, 1));
 		(*lst)->r_bytes = read(fd, (*lst)->str, BUFFER_SIZE);
 		if ((*lst)->r_bytes == -1)
-			return (free_lst(lst, 0, 1));
+			return (free_lst(lst, h, 0, 1));
 		(*lst)->str[(*lst)->r_bytes] = 0;
 		(*lst)->idx = 0;
 	}
 	return ((*lst)->str);
 }
 
-static char	*dup_or_join(t_list **lst, char *r_str, size_t n)
+static char	*dup_or_join(t_list **lst, t_list *head, char *r_str, size_t n)
 {
 	if (r_str == 0)
 		r_str = ft_strndup((*lst)->str + (*lst)->idx, n);
 	else
 		r_str = ft_strnjoin(r_str, (*lst)->str + (*lst)->idx, n);
 	if (r_str == 0)
-		return (free_lst(lst, r_str, 1));
+		return (free_lst(lst, head, r_str, 1));
 	(*lst)->idx += check_nl((*lst)->str + (*lst)->idx);
 	return (r_str);
 }
 
-static char	*str_make(t_list **lst, char *r_str)
+static char	*str_make(t_list **lst, t_list *h, char *r_str)
 {
 	while (!check_nl((*lst)->str + (*lst)->idx) && (*lst)->r_bytes != 0)
 	{
 		if ((*lst)->r_bytes == BUFFER_SIZE)
-			r_str = dup_or_join(lst, r_str, BUFFER_SIZE - (*lst)->idx);
+			r_str = dup_or_join(lst, h, r_str, BUFFER_SIZE - (*lst)->idx);
 		else
-			r_str = dup_or_join(lst, r_str, (*lst)->r_bytes - (*lst)->idx);
+			r_str = dup_or_join(lst, h, r_str, (*lst)->r_bytes - (*lst)->idx);
 		if (r_str == 0)
-			return (free_lst(lst, r_str, 1));
+			return (free_lst(lst, h, r_str, 1));
 		free((*lst)->str);
 		(*lst)->str = 0;
-		if (!set_lst(lst, (*lst)->fd))
+		if (!set_lst(lst, h, (*lst)->fd))
 		{
 			free(r_str);
 			return (0);
@@ -102,34 +108,37 @@ static char	*str_make(t_list **lst, char *r_str)
 	}
 	if (check_nl((*lst)->str + (*lst)->idx))
 	{
-		r_str = dup_or_join(lst, r_str, check_nl((*lst)->str + (*lst)->idx));
+		r_str = dup_or_join(lst, h, r_str, check_nl((*lst)->str + (*lst)->idx));
 		return (r_str);
 	}
 	if ((*lst)->r_bytes != BUFFER_SIZE)
-		r_str = dup_or_join(lst, r_str, (*lst)->r_bytes - (*lst)->idx);
+		r_str = dup_or_join(lst, h, r_str, (*lst)->r_bytes - (*lst)->idx);
 	return (r_str);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_list	*head;
-	t_list			*temp;
+	static t_list	*lst;
+	t_list			*head;
 	char			*r_str;
+	int				is_one_node;
 
-	temp = head;
+	head = lst;
+	is_one_node = 0;
 	if (fd < 0 || read(fd, 0, 0) == -1)
-	{
-		//while (temp)
-		return (free_lst(&temp, 0, 1));
-	}
+		return (free_lst(&lst, head, 0, 1));
 	r_str = 0;
-	if (!set_lst(&temp, fd))
+	if (!set_lst(&lst, head, fd))
 		return (0);
-	if (temp->str != 0 && temp->r_bytes != 0)
-		r_str = str_make(&temp, r_str);
-	if (temp != 0 && temp->str[temp->idx] == 0)
-		free_lst(&temp, 0, (temp->r_bytes < BUFFER_SIZE));
-	if (!head)
-		head = temp;
+	if (head == 0)
+		head = lst;
+	if (head->next_node == 0)
+		is_one_node = 1;
+	if (lst->str != 0 && lst->r_bytes != 0)
+		r_str = str_make(&lst, head, r_str);
+	if (lst != 0 && lst->str[lst->idx] == 0)
+		free_lst(&lst, head, 0, (lst->r_bytes == 0));
+	if ((lst != 0 && is_one_node == 0))
+		lst = head;
 	return (r_str);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:29:19 by mkong             #+#    #+#             */
-/*   Updated: 2023/11/29 21:55:49 by mkong            ###   ########.fr       */
+/*   Updated: 2023/11/30 19:53:15 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,24 @@ int main()
 	char	*s;
 
 	int fd = open("abc", O_RDONLY);
-
-	while (1)
+	int fds[3];
+	fds[0] = open("abc", O_RDONLY);
+	fds[1] = open("aa", O_RDONLY);
+	fds[2] = open("bb", O_RDONLY);
+	// while (1)
+	// {
+	for (int i = 0; i < 3; ++i)
 	{
-		s = get_next_line(fd);
-		if (!s)
-			break ;
-		printf("main  : %s \n", s);
+		s = get_next_line(fds[0]);
+		printf("main1  : %s \n", s);
 		free(s);
-		// s = get_next_line(fd);
-		// printf("main  : %s \n", s);
-		// free(s);
-		// system("leaks a.out");
+		s = get_next_line(fds[1]);
+		printf("main2  : %s \n", s);
+		s = get_next_line(fds[1]);
+		printf("main2  : %s \n", s);
+		free(s);
+		s = get_next_line(fds[2]);
+		printf("main3  : %s \n", s);
+		free(s);
 	}
 }
