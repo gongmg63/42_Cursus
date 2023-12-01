354561c5088e847fff32c1ba150c21de5330f28a
gongmg <rhdalsrn2@naver.com>
2023-12-01 21:29:03 +0900

    all

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:41:55 by mkong             #+#    #+#             */
-/*   Updated: 2023/12/01 19:12:28 by mkong            ###   ########.fr       */
+/*   Updated: 2023/12/01 21:26:34 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,12 @@
 # endif
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
-//stdio.h 지우기
+
 char	*get_next_line(int fd);
 size_t	check_nl(char *buf);
 size_t	ft_strlen(const char *s);
 char	*ft_strnjoin(char *s1, char const *s2, size_t n);
 char	*ft_strndup(const char *s1, size_t n);
-
-// typedef struct s_list
-// {
-// 	int				fd;
-// 	char			*str;
-// 	size_t			idx;
-// 	ssize_t			r_bytes;
-// 	struct s_list	*next_node;
-// }				t_list;
 
 typedef struct s_node
 {
@@ -40,12 +30,12 @@ typedef struct s_node
 	char			*buf;
 	size_t			idx;
 	struct s_node	*rnode;
-}				t_node;
+}				t_node_;
 
 typedef struct s_list
 {
-	t_node	*head;
-	t_node	*tail;
+	t_node_	*head;
+	t_node_	*tail;
 }				t_list;
 
 #endif
