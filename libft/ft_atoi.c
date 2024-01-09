4f6a8bdb7323bd3b9399f9bfa67b36598462cfc1
gongmg <rhdalsrn2@naver.com>
2024-01-09 13:14:30 +0900

    mod_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mkong <mkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 17:17:23 by mkong             #+#    #+#             */
-/*   Updated: 2024/01/05 20:49:16 by mkong            ###   ########.fr       */
+/*   Updated: 2024/01/09 13:14:06 by mkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ int	ft_atoi(const char *str)
 	result = 0;
 	sign = 1;
 	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
+		if (*str++ == '-')
 			sign = -1;
-		str++;
-	}
 	while (*str != 0)
 	{
+		result = 10 * result + *str - '0';
+		if (result == 2147483648 && sign == -1)
+		{
+			str++;
+			continue ;
+		}
 		if (!ft_isdigit(*str) || ((int)result != result))
 		{
 			write (1, "Error\n", 6);
 			exit(1);
 		}
-		result = 10 * result + *str - '0';
-		if (result == 2147483648 && sign != -1)
-			continue ;
 		str++;
 	}
 	return ((int)result * sign);
