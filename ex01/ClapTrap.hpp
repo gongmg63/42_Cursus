2e0f989c1ed173a86be4be3206486732fa1ef246
gongmg <rhdalsrn2@naver.com>
2024-05-17 13:23:55 +0900

    final

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -19,11 +19,11 @@ public:
 	ClapTrap&	operator=(const ClapTrap& copy);
 	
 	//Destructor
-	~ClapTrap	();
+	virtual ~ClapTrap	();
 
 	//member function
 	void			set_name(const std::string& name);
-	void			attack(const std::string& target);
+	virtual void	attack(const std::string& target);
 	void			takeDamage(unsigned int amount);
 	void			beRepaired(unsigned int amount);
 };
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,9 +7,9 @@ int	main()
 	ScavTrap st3(st2);
 
 	std::cout << "------------------------------------------" << std::endl;
-	st.attack("kong");
+	st.attack("min");
 	st = st2;
-	st.attack("kong");
+	st.attack("min");
 
 	std::cout << "------------------------------------------" << std::endl;
 	st2.attack("min");
@@ -17,5 +17,20 @@ int	main()
 	
 	std::cout << "------------------------------------------" << std::endl;
 	st3.takeDamage(1000);
+	st3.takeDamage(1000);
 	std::cout << "------------------------------------------" << std::endl;
+
+	ClapTrap *cs = new ClapTrap("kong");
+	ClapTrap *cs2 = new ScavTrap("kong");
+	std::cout << "------------------------------------------" << std::endl;
+	cs->attack("min");
+	cs2->attack("min");
+	std::cout << "------------------------------------------" << std::endl;
+	delete cs;
+	delete cs2;
+
+	// for (int i = 0; i < 50; ++i)
+	// 	st.beRepaired(10);
+	// st.attack("min");
+	// st.guardGate();
 }
