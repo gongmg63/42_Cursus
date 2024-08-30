8c0b7dd051aa693619ecfa34189da07f0934b39b
gongmg <rhdalsrn2@naver.com>
2024-08-30 19:04:25 +0900

    [MOD] delete test app

--- ft_transcendence/res/requirements/django/volumes/mysite/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import UsersAPIView, UserAPIView
-
-urlpatterns = [
-	path('list/', UsersAPIView.as_view()),
-	path('list/<int:id>/', UserAPIView.as_view()),
-]
