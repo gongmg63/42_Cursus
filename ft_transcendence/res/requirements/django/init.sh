887f5beb0fd016858b704648f3eac1d417ef176b
gongmg <rhdalsrn2@naver.com>
2024-09-10 14:25:32 +0900

    [ADD] webscoket connect test & friend active check

--- ft_transcendence/res/requirements/django/init.sh
+++ ft_transcendence/res/requirements/django/init.sh
@@ -4,7 +4,7 @@ set -e
 
 until psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1;" > /dev/null 2>&1; do
   echo "Waiting for PostgreSQL..."
-  sleep 6
+  sleep 3
 done
 
 python manage.py makemigrations user
@@ -15,5 +15,8 @@ python manage.py loaddata users.json
 
 # python manage.py collectstatic
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
 # uvicorn mysite.asgi:application --host 0.0.0.0 --port 8000
+# daphne -p 8000 mysite.asgi:application
+# daphne -u /tmp/daphne.sock -p 8000 --bind 0.0.0.0 mysite.asgi:application
+daphne -b 0.0.0.0 -p 8000 mysite.asgi:application
--- ft_transcendence/res/requirements/django/volumes/mysite/user/routing.py
+++ ft_transcendence/res/requirements/django/volumes/mysite/user/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/friend/status/$', consumers.FriendStatusConsumer.as_asgi()),
+    re_path(r'^ws/friend/status/$', consumers.FriendStatusConsumer.as_asgi()),
 ]
